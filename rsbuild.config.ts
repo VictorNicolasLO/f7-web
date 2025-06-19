1a90404eb773b4e31d5b2b5e4175279e9c7430a0
Victor Nicolas Lizarraga Ochoa <victornicolaslo@192.168.0.70>
2025-06-18 18:03:07 -0700

    set title

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 import { RsdoctorRspackPlugin } from '@rsdoctor/rspack-plugin';
 export default defineConfig({
+  html: {
+    title: 'F7',
+  },
   plugins: [
     pluginReact(),
     process.env.RSDOCTOR ? new RsdoctorRspackPlugin({}) as any : undefined,
